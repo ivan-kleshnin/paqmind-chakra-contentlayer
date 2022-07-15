9b2735870a89e2c2ac8c987d2e11fcf0b5f59b64
Ivan Kleshnin <ivan@paqmind.com>
2022-07-15 17:17:11 +0200

    Improve resize-on-scroll algorithm

--- layout/MainMenu.tsx
+++ layout/MainMenu.tsx
@@ -14,7 +14,11 @@ function useScrollYs(): [number[], -1 | 0 | 1] {
   // Modifying the following constants will affect fidelity
   const frameRate = 5    // -- affects slow scrolling
   const stackLength = 10 // -- affects slow scrolling
-  const threshold = 16 * 10 // in px -- affects fast scrolling
+  const thresholdDown = 16 * 10 // in px -- affects fast scrolling
+  const thresholdUp = 16 * 20 // in px -- affects fast scrolling
+  // Different constants for scrolling UP and DOWN
+  // because menu height increase is expensive while decrease isn't (position: static)
+  // The fidelity is 2x decreased for UP scrolls.
 
   // Algorithm
   const scrollY = useScrollPosition(frameRate)
@@ -32,6 +36,10 @@ function useScrollYs(): [number[], -1 | 0 | 1] {
     const min = R.reduce(R.min, Infinity, sortedYs) as number
     const max = R.reduce(R.max, 0, sortedYsRev) as number
 
+    // Close to the top
+    if (scrollY < thresholdDown) {
+      return [ys, -1]
+    }
     // For slow scrolling
     if (sortedYs.length == 10 && R.equals(ys, sortedYs)) {
       return [ys, 1]
@@ -39,9 +47,9 @@ function useScrollYs(): [number[], -1 | 0 | 1] {
       return [ys, -1]
     }
     // For fast scrolling
-    if (scrollY - min > threshold) {
+    if (scrollY - min > thresholdDown) {
       return [ys, 1]
-    } else if (max - scrollY > threshold) {
+    } else if (max - scrollY > thresholdUp) {
       return [ys, -1]
     }
     return [ys, 0]
