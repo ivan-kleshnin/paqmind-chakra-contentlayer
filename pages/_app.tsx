8a0b4070c955791b8496c82bf3e8d86fca86b1ca
Ivan Kleshnin <ivan@paqmind.com>
2022-07-18 15:47:00 +0200

    Remove custom cache (FOUC)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import {ChakraProvider} from "@chakra-ui/react"
-import {CacheProvider} from "@emotion/react"
-import createCache from "@emotion/cache"
+// import {CacheProvider} from "@emotion/react"
+// import createCache from "@emotion/cache"
 import "@fontsource/source-sans-pro/400.css"
 import "@fontsource/source-serif-pro/400.css"
 import "@fontsource/source-serif-pro/400-italic.css"
@@ -13,8 +13,8 @@ import {theme} from "lib/theme"
 import "styles/reset.css"
 import "styles/prisma.css"
 
-const cache = createCache({key: "q"})
-cache.compat = true
+// const cache = createCache({key: "q"})
+// cache.compat = true
 
 export default function MyApp({Component, pageProps}: AppProps) {
   return <>
@@ -23,11 +23,9 @@ export default function MyApp({Component, pageProps}: AppProps) {
       {/*<meta name="viewport" content="minimum-scale=1, initial-scale=1.25, width=device-width"/>*/}
     </Head>
     <ChakraProvider theme={theme}>
-      <CacheProvider value={cache}>
-        <Layout>
-          <Component {...pageProps}/>
-        </Layout>
-      </CacheProvider>
+      <Layout>
+        <Component {...pageProps}/>
+      </Layout>
     </ChakraProvider>
   </>
 }
